2ae8d5fdcf97ca6afc0083e61da651c449dea64d
Matthias Eckstaedt <matthias.eckstaedt@sap.com>
2023-01-13 13:56:01 +0100

    add export types

--- src/app/export/export.page.ts
+++ src/app/export/export.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ItemReorderEventDetail, ModalController } from '@ionic/angular';
 import * as dayjs from 'dayjs';
 import { jsPDF } from "jspdf";
 import 'jspdf-autotable';
@@ -10,28 +10,69 @@ import { DbService } from '../services/db.service';
 import { Attendance, Player } from '../utilities/interfaces';
 import { Utils } from '../utilities/Utils';
 
+const DEFAULT_PLAYER_FIELDS = ["Vorname", "Nachname", "Geburtsdatum", "Instrument"];
+const DEFAULT_ATT_FIELDS = ["Vorname", "Nachname", "Instrument"];
+
 @Component({
   selector: 'app-export',
   templateUrl: './export.page.html',
   styleUrls: ['./export.page.scss'],
 })
+
 export class ExportPage implements OnInit {
   public type: string = "pdf";
   public players: Player[] = [];
   public content: string = "player";
   public attendance: Attendance[] = [];
+  public selectedFields: string[] = DEFAULT_PLAYER_FIELDS;
+  public fields: string[] = ["Vorname", "Nachname", "Geburtsdatum", "Instrument"];
 
   constructor(
     private modalController: ModalController,
     private db: DbService,
+    private alertController: AlertController,
   ) { }
 
   async ngOnInit() {
     this.players = Utils.getModifiedPlayers(await this.db.getPlayers(), await this.db.getInstruments());
+    this.attendance = (await this.db.getAttendance()).filter((att: Attendance) => dayjs(att.date).isBefore(dayjs().startOf("day")));
   }
 
   dismiss() {
     this.modalController.dismiss();
+  }
+
+  onSegmentChange() {
+    this.selectedFields = this.content === "player" ? DEFAULT_PLAYER_FIELDS : DEFAULT_ATT_FIELDS;
+  }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    ev.detail.complete(this.selectedFields);
+  }
+
+  removeField(index: number, slider: HTMLIonItemSlidingElement) {
+    this.selectedFields.splice(index, 1);
+    slider.close();
+  }
+
+  async addExtraField() {
+    const alert = await this.alertController.create({
+      header: 'Feld hinzufügen',
+      inputs: [{
+        type: "text",
+        name: "field"
+      }],
+      buttons: [{
+        text: "Abbrechen"
+      }, {
+        text: "Hinzufügen",
+        handler: (evt: any) => {
+          this.selectedFields.push(evt.field);
+        }
+      }]
+    });
+
+    await alert.present();
   }
 
   export() {
@@ -46,11 +87,10 @@ export class ExportPage implements OnInit {
     let row = 1;
 
     const date: string = dayjs().format('DD.MM.YYYY');
-    const data = [['', 'Nachname', 'Vorname', 'Instrument', 'Geburtsdatum']];
+    const data = [['', ...this.selectedFields]];
 
     for (const user of this.players) {
-      const birthday: string = dayjs(user.birthday).format('DD.MM.YYYY');
-      data.push([row.toString(), user.firstName, user.lastName, user.instrumentName, birthday]);
+      data.push([row.toString(), ...this.getFieldValues(user)]);
       row++;
     }
 
@@ -68,15 +108,14 @@ export class ExportPage implements OnInit {
     const data = [];
 
     for (const user of this.players) {
-      const birthday: string = dayjs(user.birthday).format('DD.MM.YYYY');
-      data.push([row.toString(), user.firstName, user.lastName, user.instrumentName, birthday]);
+      data.push([row.toString(), ...this.getFieldValues(user)]);
       row++;
     }
 
     const doc = new jsPDF();
     doc.text(`${environment.shortName} Spielerliste Stand: ${date}`, 14, 25);
     ((doc as any).autoTable as AutoTable)({
-      head: [['', 'Vorname', 'Nachname', 'Instrument', 'Geburtsdatum']],
+      head: [['', ...this.selectedFields]],
       body: data,
       margin: { top: 40 },
       theme: 'grid',
@@ -98,31 +137,28 @@ export class ExportPage implements OnInit {
     const attDates: string[] = [];
     const attPerc: string[] = [];
     const data = [];
-    const players: Player[] = Utils.getModifiedPlayers((await this.db.getPlayers()), (await this.db.getInstruments()));
 
     for (const att of attendance) {
       attDates.push(dayjs(att.date).format('DD.MM.YY'));
       attPerc.push(String(att.percentage ? `${att.percentage}%` : ""));
     }
 
-    for (const player of players) {
+    for (const player of this.players) {
       const attInfo: string[] = [];
 
       for (const att of attendance) {
         if (att.players[player.id] !== undefined) {
-          attInfo.push(att.players[player.id] ? "X" : "V");
+          attInfo.push(att.players[player.id] ? "X" : att.excused.includes(player.id.toString()) ? "E" : "V");
         } else {
           attInfo.push("");
         }
       }
 
-      data.push([row.toString(), player.firstName, player.lastName, player.instrumentName, ...attInfo.reverse()]);
+      data.push([row.toString(), ...this.getFieldValues(player), ...attInfo.reverse()]);
       row++;
     }
 
-    data.push(["", "", "", "", ...attPerc.reverse()]);
-
-    const header: string[] = ['', 'Nachname', 'Vorname', 'Instrument', ...attDates.reverse()];
+    const header: string[] = ['', ...this.selectedFields, ...attDates.reverse()];
 
     if (this.type === "excel") {
       data.unshift(header)
@@ -173,12 +209,42 @@ export class ExportPage implements OnInit {
           data.cell.styles.fillColor = [50, 205, 50];
           data.cell.styles.textColor = [255, 255, 255];
           data.cell.styles.halign = "center";
-        } else if (data.cell.raw.toString().includes("%")) {
+        } else if (data.cell.raw === "E") {
+          data.cell.styles.fillColor = [255, 196, 9];
+          data.cell.styles.textColor = [255, 255, 255];
+          data.cell.styles.halign = "center";
+        } else if (data.cell.raw?.toString().includes("%")) {
           data.cell.styles.halign = "center";
         }
       },
     });
     doc.save(`${environment.shortName}_Anwesenheit_Stand_${date}.pdf`);
+  }
+
+  getFieldValues(player: Player) {
+    const values: string[] = [];
+
+    for (const field of this.selectedFields) {
+      switch (field) {
+        case "Vorname":
+          values.push(player.firstName);
+          break;
+        case "Nachname":
+          values.push(player.lastName);
+          break;
+        case "Geburtsdatum":
+          values.push(dayjs(player.birthday).format('DD.MM.YYYY'));
+          break;
+        case "Instrument":
+          values.push(player.instrumentName);
+          break;
+        default:
+          values.push("");
+          break;
+      }
+    }
+
+    return values;
   }
 
 }
