41a9121a04877f88257dbc36ffc588eaffae28b7
Lionhunt3r <leonjaeger00@gmail.com>
2023-09-27 18:58:04 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,7 +64,7 @@ export class AppComponent {
               role: 'cancel'
             }, {
               text: "Aktualisieren",
-              handler: (values: any) => {
+              handler: () => {
                 document.location.reload();
               }
             }
